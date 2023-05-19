cd35f751a11b3d03214fedcdb93b56a4e4752c6f
Naveen Hettiwaththa <naveenhettiwaththa@gmail.com>
2023-05-19 16:23:55 +0530

    added user auth required middleware

---
+++ src/middlewares/UserAuthRequired.middleware.js
@@ -0,0 +1,44 @@
+const jwt = require("jsonwebtoken");
+// @ts-ignore
+const User = require("../models/User.model");
+// @ts-ignore
+const AuthToken = require("../models/AuthToken.model");
+
+const UserAuthRequired = () => {
+  return async (req, res, next) => {
+    const token = req.headers["authorization"];
+
+    if (!token) {
+      return res.status(401).send({ error: "Unauthenticated" });
+    }
+
+    // in future check if token is in blacklist(logged out before token expired)
+    try {
+      let checkExists = await AuthToken.findOne({ token: token });
+
+      if (!checkExists) {
+        return res.status(401).send({ error: "Unauthenticated" });
+      }
+    } catch (err) {
+      return res.status(401).send({ error: "Unauthenticated" });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, async (err, userObject) => {
+      if (err) return res.status(403).json({ message: "Unauthenticated" });
+
+      try {
+        let user = await User.findOne({ email: userObject.email }).select(
+          "-password"
+        );
+
+        req.user = user;
+
+        next();
+      } catch (err) {
+        return res.status(401).send({ error: "Unauthenticated" });
+      }
+    });
+  };
+};
+
+module.exports = UserAuthRequired;
