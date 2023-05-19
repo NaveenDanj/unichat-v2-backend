46d6ce61f66965f39b24aa16b9549a7c4b83e9cc
Naveen Hettiwaththa <naveenhettiwaththa@gmail.com>
2023-05-19 17:52:38 +0530

    added contact model

---
+++ src/models/Contact.model.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+
+    contactUser: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+
+    contactName: {
+      type: String,
+      required: false,
+    },
+
+    roomId: {
+      type: String,
+      required: false,
+    },
+
+    blocked: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+
+    starred: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  { timestamps: true, strict: false }
+);
+
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
